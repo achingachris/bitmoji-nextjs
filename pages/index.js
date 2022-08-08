ad7ade8e95e92d328f556735a884bb139508594e
chrisachinga <achinga.chris@gmail.com>
2022-08-09 00:13:01 +0300

    fixing errors

--- pages/index.js
+++ pages/index.js
@@ -12,41 +12,6 @@ const index = () => {
 
       {/* form */}
       <ImageUploadForm />
-
-      <div className='container mt-4'>
-        <div className='row'>
-          <div className='col-xl-6 col-md-6 mb-6'>
-            <div className='card border-0 shadow'>
-              <Image
-                src='/image1.jpeg'
-                className='card-img-top'
-                width='500'
-                height='350'
-                alt='...'
-              />
-              <div className='card-body text-center'>
-                <h5 className='card-title mb-0'>User Image</h5>
-                <div className='card-text text-black-50'>Web Developer</div>
-              </div>
-            </div>
-          </div>
-          <div className='col-xl-6 col-md-6 mb-6'>
-            <div className='card border-0 shadow'>
-              <Image
-                src='/image2.png'
-                className='card-img-top'
-                width='500'
-                height='350'
-                alt='...'
-              />
-              <div className='card-body text-center'>
-                <h5 className='card-title mb-0'>Bitmoji</h5>
-                <div className='card-text text-black-50'>Web Developer</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
